2319d373e993cfc487e205c321eff818124393e1
theHimanshuShekhar <hshekhar.dev@gmail.com>
2022-11-25 23:05:59 +0530

    bug fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "media",
-  content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
